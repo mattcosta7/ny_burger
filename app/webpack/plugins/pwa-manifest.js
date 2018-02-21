6822278b2fdc9cbc07c333a9dbeb75112df1b03d
Matthew Costabile <mattcosta7@gmail.com>
2018-02-20 23:51:45 -0500

    updates pwa

--- app/webpack/plugins/pwa-manifest.js
+++ app/webpack/plugins/pwa-manifest.js
@@ -2,12 +2,19 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = new WebpackPwaManifest({
+  publicPath: process.env.NODE_ENV === 'production' ? '//d3fels6wqsoo93.cloudfront.net/' : '/',
   filename: 'manifest.json',
-  name: 'My Progressive Web App',
-  short_name: 'MyPWA',
-  description: 'My awesome Progressive Web App!',
+  name: 'New York Burger Blog',
+  short_name: 'NY Burger Blog',
+  description:
+    'Taking on NYC one Burger at a time. With a cast of Burger experts telling you what you need to put in your mouth across the city.',
   background_color: '#ffffff',
-  start_url: '/',
+  start_url: 'https://www.nyburgerblog.com/',
+  theme_color: '#063861',
+  fingerprints: true,
+  ios: true,
+  orientation: 'portrait',
+  display: 'standalone',
   icons: [
     {
       src: path.resolve('./src/assets/favicon/android-chrome-144x144.png'),
