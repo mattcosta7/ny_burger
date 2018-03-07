3708f79ee959e0933373dbb3759f1cc2a2e232ab
Matthew Costabile <mattcosta7@gmail.com>
2018-03-07 00:58:01 -0500

    fix ga

--- app/src/components/Analytics/index.jsx
+++ app/src/components/Analytics/index.jsx
@@ -5,12 +5,10 @@ import { NODE_ENV, DEVELOPMENT_ENV } from '../../../config';
 
 class Analytics extends React.Component {
   componentDidMount() {
-    if (NODE_ENV === DEVELOPMENT_ENV) return;
     ReactGA.initialize('UA-115326879-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
   }
   componentDidUpdate(lastProps) {
-    if (NODE_ENV === DEVELOPMENT_ENV) return;
     if (lastProps.location.pathname !== this.props.location.pathname) {
       ReactGA.set({ page: this.props.location.pathname });
       ReactGA.pageview(this.props.location.pathname);
